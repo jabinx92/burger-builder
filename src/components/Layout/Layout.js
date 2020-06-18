367c7af05d7dadbf71d07ecbb8b54a6eb8d73b09
Jonathan Won <jabinx92@gmail.com>
2020-06-17 17:36:50 -0700

    mid

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
+import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
-console.log(React.version);
 
 const layout = (props) => (
-    <div>
-
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-        {props.children}
-    </main>
-    </div>
+    <Aux>
+        <Toolbar />
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
     
 );
 
